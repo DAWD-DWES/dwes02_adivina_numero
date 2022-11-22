a68ab1decb61c4840b6e58f188525ba0c23959a5
Iván <jivanrod@gmail.com>
2022-11-22 18:23:27 +0100

    update

--- index.php
+++ index.php
@@ -7,13 +7,13 @@ define('UPPER_BOUND', 20);
 if (empty($_POST) || isset($_POST['newgamebutton'])) { // Si se arranca el juego o se solicita una nueva partida
     $numTries = 0;
     $numHidden = mt_rand(LOWER_BOUND, UPPER_BOUND); // Genero un valor aleatorio
-    $numeros = []; // Array de números jugados
+    $numbers = []; // Array de números jugados
 } else { // Si estoy en mitad del juego leo los valores del formulario
     $numHidden = filter_input(INPUT_POST, 'num_hidden', FILTER_SANITIZE_NUMBER_INT);
     $numTries = filter_input(INPUT_POST, 'num_tries', FILTER_SANITIZE_NUMBER_INT);
     $guess = filter_input(INPUT_POST, 'guess', FILTER_SANITIZE_NUMBER_INT);
-    $numeros = filter_input(INPUT_POST, 'numeros', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
-    $numeros[] = $guess;
+    $numbers = filter_input(INPUT_POST, 'numbers', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY);
+    $numbers[] = $guess;
     ++$numTries;
     $end = $numTries >= MAX_TRIES || $guess === $numHidden; // Establezco si se ha acabado la partida o no
 }
@@ -31,10 +31,13 @@ if (empty($_POST) || isset($_POST['newgamebutton'])) { // Si se arranca el juego
         <div class="page">
             <h1>Guess Hidden Number!</h1>
             <div class="capaform">
-                <form class="form-font" name="form_guessnumber" 
+                <form class="form" name="form_guessnumber" 
                       action="index.php" method="POST">
                     <input type="hidden" name="num_hidden" value="<?= $numHidden ?>" /> <!-- Incluyo el número secreto en el formulario para que no se pierda -->
                     <input type="hidden" name="num_tries" value="<?= $numTries ?>" /> <!-- Incluyo el número de intentos en el formulario para que no se pierda -->
+                    <?php foreach ($numbers as $number): ?>
+                        <input type="hidden" name="numbers[]" value="<?= $number ?>" />
+                    <?php endforeach ?>
                     <div class="form-section">
                         <div class="input-section">
                             <label for="guess"><?= 'Enter a number (' . LOWER_BOUND . '-' . UPPER_BOUND . '):' ?></label> 
@@ -56,17 +59,17 @@ if (empty($_POST) || isset($_POST['newgamebutton'])) { // Si se arranca el juego
                         <?php endif ?>
                     </div>
                     <?php if (isset($end) && !$end): ?>
-                        <p class="info-section">Tries left: <?= MAX_TRIES - $numTries ?></p>
-                        <p class="info-section"><?= ($guess <=> $numHidden) > 0 ? 'Try with a lower number' : 'Try with a higher number' ?></p>
+                        <div class="info-section">
+                            <p>Tries left: <?= MAX_TRIES - $numTries ?></p>
+                            <p><?= ($guess <=> $numHidden) > 0 ? 'Try with a lower number' : 'Try with a higher number' ?></p>
+                            <p>You already played the following numbers: <?= implode(",", $numbers) ?></p>
+                        </div>
                     <?php endif ?>
                     <?php if (isset($end) && $end): ?>
                         <p class="info-section"><?= ($guess === $numHidden) ? "Well done!!! Got it in {$numTries} tries" : 'You lost!!' ?></p>
                     <?php endif ?>
+                </form> 
             </div>
-        </form> 
-    </div>
-</div>  
-</div>
-</div>
-</body>
+        </div>  
+    </body>
 </html>
