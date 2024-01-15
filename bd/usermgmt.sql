a0cd8571a55483547ef81a139eac06cffcc66c41
Iván <jivanrod@gmail.com>
2024-01-15 12:23:52 +0100

    Update usermgmt.sql

--- bd/usermgmt.sql
+++ bd/usermgmt.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
-  `email` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(60) COLLATE utf8_spanish_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
